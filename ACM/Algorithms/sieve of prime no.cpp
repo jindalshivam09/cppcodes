c2913c4d3e144de7a0a60749b675e2f661d5b07b
Shivam Jindal <jindalshivam09@gmail.com>
2020-07-26 11:45:42 -0700

    Update sieve of prime no.cpp

--- ACM/Algorithms/sieve of prime no.cpp
+++ ACM/Algorithms/sieve of prime no.cpp
@@ -16,16 +16,17 @@ void sieve(int n) {
 	for(int i=2;i<=s;i++)
 	{
 		a=i;
-		if(!isPrime[i])
-    	for(int j=a*a;j<=n;j+=2*i)
+		if(!isPrime[i]) {
+			for(int j=a*a;j<=n;j+=2*i)
 			   isPrime[j]=1;
+		}
 	}
+	
 	int j=2;
 	prime[1] = 2;
-	for(int i=3;i<=n;i+=2)
-	if(!isPrime[i])
-		prime[j++] = i;
-	cout << j << endl;
+	for(int i=3;i<=n;i+=2) {
+		if(!isPrime[i])	 prime[j++] = i;
+	}
 }
 
 main()
